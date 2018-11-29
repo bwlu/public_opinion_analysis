f8088f2fef1fe5a06d20a27afcedb8d6e7e8167b
spring-qili <liu2461875094@163.com>
2018-11-29 20:04:19 +0800

    数据采集（爬虫+消息队列）代码

---
+++ baiduspiderProject_new/baiduspider/together.py
@@ -0,0 +1,33 @@
+import os
+import time
+
+while True:
+   os.system("scrapy crawl sbaidu")
+   time.sleep(10)
+   os.system("scrapy crawl hhtcs")
+   timesleep(10)
+   os.system("scrapy crawl hhtgz")
+   time.sleep(10)
+   os.system("scrapy crawl hhtjl")
+   time.sleep(10)
+   os.system("scrapy crawl hhtxq")
+   time.sleep(10)
+   os.system("scrapy crawl jdwxbz")
+   time.sleep(10)
+   os.system("scrapy crawl jdwxgg")
+   time.sleep(10)
+   os.system("scrapy crawl jdwxgq")
+   time.sleep(10)
+   os.system("scrapy crawl jdwxts")
+   time.sleep(10)
+   os.system("scrapy crawl wszgcs")
+   time.sleep(10)
+   os.system("scrapy crawl wszgds")
+   time.sleep(10)
+   os.system("scrapy crawl wszgjd")
+   time.sleep(10)
+   os.system("scrapy crawl wszgzh")
+   time.sleep(10)
+   os.system("scrapy crawl wxgzh")
+   os.time.sleep(1000)
+   

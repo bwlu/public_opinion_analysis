05c00f6cbaaaa75936ae8f774c3c11f375a6ebcc
spring-qili <liu2461875094@163.com>
2018-11-29 18:48:08 +0800

    数据采集（爬虫+消息队列）代码
    数据采集（爬虫+消息队列）代码

---
+++ baiduspiderProject_new/baiduspider/items.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class BaiduspiderItem(scrapy.Item):
+    # define the fields for your item here like:
+    title = scrapy.Field()
+    info = scrapy.Field()
+    time = scrapy.Field()
+    UrlId = scrapy.Field()
+    childPage = scrapy.Field()
+    IsLimitedTime = scrapy.Field()#是否超过时间限制
+    #读取帖子内容的属性

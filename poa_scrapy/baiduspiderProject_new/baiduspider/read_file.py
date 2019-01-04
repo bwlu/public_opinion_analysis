f53b9fecc33e1e51a1483234d1ac4f245dde4074
bwlu <18232386357@163.com>
2019-01-04 11:18:52 +0800

    readfile.py

--- poa_scrapy/baiduspiderProject_new/baiduspider/read_file.py
+++ poa_scrapy/baiduspiderProject_new/baiduspider/read_file.py
@@ -1,4 +1,13 @@
 import json
+def write_file(name,list):
+    f = open('./jsonfile/'+name+'_UrlList.json', "w", encoding='UTF-8')
+    content = json.dumps(list, ensure_ascii=False)
+    f.write(content)
+    f.close()
 def read_file(name):
-    f = open('./jsonfile/'+name+'_UrlList.json', "r", encoding='UTF-8')  # 读取josn中的上次的链接
-    return json.load(f)  # 将数据存入列表
+    try:
+	    f = open('./jsonfile/'+name+'_UrlList.json', "r", encoding='UTF-8')  # 读取josn中的上次的链接
+	    return json.load(f)  # 将数据存入列表
+    except:
+    	write_file(name,[])
+

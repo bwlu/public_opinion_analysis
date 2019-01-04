bee295672d74bfd9db50477c56eb33476bf98a91
bwlu <18232386357@163.com>
2019-01-04 20:11:02 +0800

    微信启动脚本

--- start-weixin.sh
+++ start-weixin.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo '启动微信爬虫'
-scrapy='sougouweixin.py'
+scrapy='run_weixin.py'
 ps -fe|grep $scrapy |grep -v grep
 if [ $? -ne 0 ]
 then
 cd poa_scrapy
-python3 sougouweixin.py
+python3 run_weixin.py
 else
 echo "微信爬虫正在运行中......"
 fi

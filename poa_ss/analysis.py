9106314d1931d42acfc33996b82b3e467f9c6f2d
bwlu <18232386357@163.com>
2019-02-24 15:33:04 +0800

    数据源错误处理

--- poa_ss/analysis.py
+++ poa_ss/analysis.py
@@ -5,8 +5,12 @@ import json
 from pyspark.streaming.kafka import KafkaUtils
 # from pos_data import *
 from oraclepool import (basd_info_add,sendPartition,ayls_sentence,filter_sentence)
+import py4j
+import time
+import os
 
 try:
+	restart = True
 	# sc = SparkContext("local[*]", "analysis")
 	sc = SparkContext(appName="analysis")
 	# sc.setLogLevel("DEBUG")
@@ -28,9 +32,26 @@ try:
 	ssc.start()
 	ssc.awaitTermination()
 
+except py4j.protocol.Py4JJavaError as e:
+	print('数据源错误，请检查kafka服务器是否运行正常，然后重启，已自动重启，若启动失败，请手动重启')
+	log_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+	with open('../ERROR.log','w') as fp:
+		fp.write('%s\n'%(log_time))
+		fp.write('数据源错误，请检查kafka服务器是否运行正常，然后重启，已自动重启，若启动失败，请手动重启')
+	if restart:
+		restart = False
+		print('10s后重启')
+		time.sleep(10)
+		os.system("./restart.sh")
+
 except Exception as e:
 	print('**error**'*10)
 	print(e)
 	print('**error**'*10)
+	if restart:
+		restart = False
+		print('20s后重启')
+		time.sleep(20)
+		os.system("./restart.sh")
 
 
